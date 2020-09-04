abcc02c358fa661993a53981aec66f91b06ef305
Raffaele Viggiani <raffaele.viggiani@gmail.com>
2020-09-04 16:42:34 +0200

    Final changes
    - navbar
    - session
    - cases
    - some other things

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -10,6 +10,9 @@ skip_before_action :authenticate_user!, only: [ :show, :result ]
     @origin = Country.find(params[:country][:origin])
     @destination = Country.find(params[:country][:destination])
 
+    session[:create_trip_destination_id] = @destination.id
+    session[:create_trip_origin_id] = @origin.id
+
     @destination_open = Indication.where(country: @destination).find_by(name: "open")
     @destination_high_risk_areas = Indication.where(country: @destination).find_by(name: "high_risk_areas")
     @destination_quarantine = Indication.where(country: @destination).find_by(name: "quarantine")
@@ -17,6 +20,7 @@ skip_before_action :authenticate_user!, only: [ :show, :result ]
 
     @destination_restaurants = Indication.where(country: @destination).find_by(name: "restaurants")
     @destination_bars_cafes = Indication.where(country: @destination).find_by(name: "bars_cafes")
+    @destination_new_cases = Case.where(country: @destination).last.new_cases
 
     @origin_open = Indication.where(country: @origin).find_by(name: "open")
     @origin_quarantine = Indication.where(country: @origin).find_by(name: "quarantine")
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,17 +5,15 @@ before_action :set_trip, only: [ :show, :destroy ]
     # get trip variables from hidden for or not hidden form if user already sign in
     @trip = Trip.new(trip_params)
     # check if user is signed in
-      # if yes create new trip
-      @trip.user = current_user
-      if @trip.save
-        redirect_to trip_path(@trip)
-      else
-        render 'pages/home'
-      end
-    # else save origin and destination in session variable
-    # option think about find or initialize by
-    # assign user_id to trips if user is logged in
+    # if yes create new trip
 
+    @trip.user = current_user
+    if @trip.save
+      redirect_to trips_path
+    else
+      render 'pages/home'
+    end
+    # else save origin and destination in session variable
   end
 
   def index
