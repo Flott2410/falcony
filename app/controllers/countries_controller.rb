733ddce85ef0fe0cc795af7d788be28b6c5c6512
Florian Ott <florian.ott@gmx.at>
2020-09-06 16:55:14 +0100

    Continue with show page
    - added if statements to check if data is present, if not don`t
    show it
    - added if statements to controller methods to not have errors
    when data from parsing is nil

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -57,20 +57,32 @@ skip_before_action :authenticate_user!, only: [ :show, :result ]
     # Select all cases of the country of the last 14 days and build average
     cases_per_country_last_two_weeks = Case.where(country: @country).where(date: 14.days.ago..@update_date)
     cases_per_country_last_two_weeks.each do |item|
-      sum_1 += item.new_cases
+      if item.new_cases == nil
+        sum_1 = 0
+      else
+        sum_1 += item.new_cases
+      end
     end
     @last_two_weeks_average_new_cases = sum_1 / 14
 
     # Check if average of week - 2 is higher or lower than week -1
     cases_per_country_last_week = Case.where(country: @country).where(date: 7.day.ago..@update_date)
     cases_per_country_last_week.each do |item|
-      sum_2 += item.new_cases
+      if item.new_cases == nil
+        sum_2 = 0
+      else
+        sum_2 += item.new_cases
+      end
     end
     cases_per_country_last_week_avg = sum_2 / 7
 
     cases_per_country_prelast_week = Case.where(country: @country).where(date: 7.day.ago..14.day.ago) #.select()select update_date - 7 to - 14 days
     cases_per_country_prelast_week.each do |item|
-      sum_3 += item.new_cases
+      if item.new_cases == nil
+        sum_3 = 0
+      else
+        sum_3 += item.new_cases
+      end
     end
     cases_per_country_prelast_week_avg = sum_3 / 7
 
@@ -90,7 +102,11 @@ skip_before_action :authenticate_user!, only: [ :show, :result ]
     # new tests average from latest week
     new_tests_last_week = Case.where(country: @country).where(date: 7.day.ago..@update_date)
     new_tests_last_week.each do |item|
-      sum_4 += item.new_tests
+      if item.new_tests == nil
+        sum_4 = 0
+      else
+        sum_4 += item.new_tests
+      end
     end
     new_tests_last_week_avg_full = sum_4 / 7
     @new_tests_last_week_avg = new_tests_last_week_avg_full.round(2)
