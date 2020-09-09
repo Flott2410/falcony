6fe77a8a87a5173d69d3d00700ca7e0e95270553
Eukleyv Cardoso <eukleyvcardoso@gmail.com>
2020-09-09 22:38:15 +0100

    Remove unnecessary line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  include MetaTagsHelper
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :create_trip_from_session
