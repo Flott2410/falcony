ca828f5883407281b6e5933b72f72540178baa4d
Florian Ott <florian.ott@gmx.at>
2020-09-16 16:01:39 +0100

    added more tasks/job to update background job

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,4 +1,10 @@
 class Case < ApplicationRecord
   belongs_to :country
+  after_commit :async_update
 
+  private
+
+  def async_update
+    UpdateCasesJob.perform_later(self)
+  end
 end
---
+++ test/jobs/update_cases_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UpdateCasesJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
